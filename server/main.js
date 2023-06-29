18ea98dfc37e12381b0bd88b4d33798eba575312
mk3058 <kmk3058@icloud.com>
2023-06-29 10:39:54 +0900

    feat(swagger): add swagger settings

--- server/main.js
+++ server/main.js
@@ -2,12 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const pool = require('./connection');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
+const newsRouter = require('./news');
+const myNewsRouter = require('./my_news');
+const commentRouter = require('./comment');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/news', newsRouter);
+app.use('/mynews', myNewsRouter);
+app.use('/comment', commentRouter);
+
+const swaggerOptions = {
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+		title: 'LittleBit Backend Api',
+		description: 'SWUniv News Service API Server',
+		servers:['http://localhost:3000']
+		}
+	},
+	apis: ['./news.js', './my_news.js', './comment.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/swagger/apis', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- server/news.js
+++ server/news.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./connection');
 
+/**
+ * @swagger
+ * /news:
+ *   get:
+ *     summary: Get a list of news
+ *     responses:
+ *       200: 
+ *         description: news list (OK)
+ */
 router.get('/', (req, res) => {
   pool.query('SELECT * FROM news ORDER BY id', (error, results) => {
     if (error) {
@@ -12,6 +21,24 @@ router.get('/', (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /news/{id}:
+ *   get:
+ *     summary: Get a single news with {id}
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the news to retrieve.
+ *     responses:
+ *       200:
+ *         description: A single news (OK).
+ *       400:
+ *         description: Invalid ID.
+ *       404:
+ *         description: News not found.
+ */
 router.get('/:id', (req, res) => {
   const id = parseInt(req.params.id);
   if (isNaN(id)) {
@@ -30,6 +57,39 @@ router.get('/:id', (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /news/new:
+ *   post:
+ *     summary: Create a new news
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               summary:
+ *                 type: string
+ *               link:
+ *                 type: string
+ *               published_at:
+ *                 type: string
+ *                 format: date-time
+ *     responses:
+ *       201:
+ *         description: News created (OK).
+ *       400:
+ *         description: Invalid input, object invalid.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.post('/new', (req, res) => {
   const { title, author, category, summary, link, published_at } = req.body;
 
