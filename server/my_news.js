3b04c964efc1fc6dcda6a59a00fa3de2461d156c
mk3058 <kmk3058@icloud.com>
2023-06-29 10:39:54 +0900

    feat(swagger): add swagger settings

--- server/my_news.js
+++ server/my_news.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./connection');
 
+/**
+ * @swagger
+ * /mynews:
+ *   get:
+ *     summary: Get a list of MyNews
+ *     responses:
+ *       200: 
+ *         description: MyNews list (OK)
+ */
 router.get('/', (req, res) => {
   pool.query('SELECT * FROM my_news ORDER BY id DESC', (error, results) => {
     if (error) {
@@ -12,6 +21,24 @@ router.get('/', (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /mynews/{id}:
+ *   get:
+ *     summary: Get a single MyNews with {id}
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the Mynews to retrieve.
+ *     responses:
+ *       200:
+ *         description: A single Mynews (OK).
+ *       400:
+ *         description: Invalid ID.
+ *       404:
+ *         description: MyNews not found.
+ */
 router.get('/:id', (req, res) => {
   const id = parseInt(req.params.id);
   if (isNaN(id)) {
@@ -30,6 +57,32 @@ router.get('/:id', (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /mynews/new:
+ *   post:
+ *     summary: Create a new MyNews
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *               body:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: MyNews created (OK).
+ *       400:
+ *         description: Invalid input, object invalid.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.post('/new', (req, res) => {
 	const { title, author, body } = req.body;
   
@@ -48,6 +101,26 @@ router.post('/new', (req, res) => {
 	});
   });
 
+  /**
+ * @swagger
+ * /mynews/like/{id}:
+ *   put:
+ *     summary: Increment the like count of a MyNews with {id}
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the MyNews to like.
+ *     responses:
+ *       200:
+ *         description: MyNews liked (OK).
+ *       400:
+ *         description: Invalid ID.
+ *       404:
+ *         description: MyNews not found.
+ *       500:
+ *         description: Internal Server Error.
+ */
 router.put('/like/:id', (req, res) => {
 	const id = parseInt(req.params.id);
 	if (isNaN(id)) {
