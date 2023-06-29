7a5dd286c4ca6b2cd64217bdca55600da3079252
jinhere <kb081434@khu.ac.kr>
2023-06-29 16:10:58 +0900

    4:10

--- webcrawler.py
+++ webcrawler.py
@@ -1,22 +1,40 @@
 import requests
 import feedparser
+import re
+
+def is_positive(title): 
+    # result=clova 함수 결과 반환.
+    # return result
+    return True
+
+def post(positive_list): # db에 적재
+    pass
+
 
 def crawl_rss_feed(feed_url):
-    # RSS 피드 파싱
-    feed = feedparser.parse(feed_url)
-    print(feed.feed)
-    print("Feed Title:", feed.feed.title)
-    print("Feed Description:", feed.feed.description)
-    print("Feed Link:", feed.feed.link)    # 피드 아이템 순회
-    for entry in feed.entries[0:3]:
-        print(entry)
-        print("\nEntry Title:", entry.title)
-        print("Entry Link:", entry.link)
-        print("Entry Description:", entry.description)
-        print("Entry Published Date:", entry.published_parsed.tm_mday)
-        print("Category: ",entry.category)
+    positive_list=[]
+    feed = feedparser.parse(feed_url) # RSS 피드 파싱
+    # feed.feed.title
+    # print(feed)  
+    for entry in feed.entries: # title, author, category, summary,link, published_at
+        if is_positive(entry.title):
+            positive_news={}
+            keys=["title","category","summary","published_at","link"]
+            values=[entry.title,entry.category,entry.description,entry.published]
+            positive_list.append(dict(zip(keys,values)))
+            
 
-# 크롤링할 RSS 피드 URL
-rss_url = "https://www.mk.co.kr/rss/30100041/"
-# 피드 크롤링 실행
-crawl_rss_feed(rss_url)
+    return positive_list
+
+
+
+rss_url='https://www.mk.co.kr/rss/40200003/'
+positive_list=crawl_rss_feed(rss_url)      
+
+def crawl_everyday(rss_list):
+    for rss in rss_list:
+        crawl_rss_feed(rss)
+
+# main code 
+#rss_list=[]
+#crawl_everyday(rss_list)
